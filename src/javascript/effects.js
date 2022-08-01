102947fb0e6f85d032300e65ad1268a419cb37b7
JohnnyKyr <66531087+JohnnyKyr@users.noreply.github.com>
2022-08-01 20:20:17 +0300

    Add files via upload

---
+++ src/javascript/effects.js
@@ -0,0 +1,25 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+document.addEventListener("mousemove", parallax)
+function parallax(e){
+    document.querySelectorAll("img").forEach(function(move){
+
+        var moving_value = move.getAttribute("data-value");
+        var x = (e.clientX * moving_value) / 150;
+        var y = (e.clientY * moving_value) / 90;
+
+    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    });
+}
