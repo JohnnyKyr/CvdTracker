e8cb82c64131caf2e6f2c3bab618a78e5f3fee5e
JohnnyKyr <66531087+JohnnyKyr@users.noreply.github.com>
2022-08-04 01:40:38 +0300

    Minor Changes

--- src/javascript/map.js
+++ src/javascript/map.js
@@ -30,6 +30,12 @@ map_form.map_submit.addEventListener('click', (event) =>{
     request.send(requestData);
 });
 
+//ADD Place for past 2hours
+//ADD Poi esimations for next two hours
+//Fetch Name
+//Fetch rating
+
+
 let markerLayer = L.featureGroup().addTo(map);
 let popup = L.popup()
 .setContent("I am a standalone popup.");
@@ -37,9 +43,7 @@ let popup = L.popup()
 let marker = Array();
 
 function HandleResponse(responseObject){
-   let data  =new Date();
    
-    console.log(data.getHours());
     for (let i = 0; i < responseObject.lat.length; i++) {
         marker[i] = {"lat":responseObject.lat[i],"lng": responseObject.lng[i], "id":responseObject.id[i]}
         L.marker([responseObject.lat[i],responseObject.lng[i]]).addTo(markerLayer).bindPopup(popup);
@@ -63,17 +67,23 @@ markerLayer.addEventListener("click",(event)=>{
         }
 
         if (responseObject){
-            HandleResponse(responseObject);
+            MarkerResponse(responseObject);
         }
     };
 
 
-    const requestData = `id =${marker_id}`;
+    const requestData = `id=${marker_id}`;
     
-    request.open('post', '../php/popularityHandle.php');
+    request.open('post', '../php/popUpHandle.php');
     request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-    request.send(requestData);
+    request.send();
    
 });
 
 
+function MarkerResponse(responseObject){
+    for (let i = 0; i < responseObject.name.length; i++) {
+        console.log(responseObject.name[i])
+      } 
+
+}
